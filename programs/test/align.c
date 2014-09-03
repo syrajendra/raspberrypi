8700923e0468ef72f44eac88a461d9b84b7efd9f
syrajendra <rajendra.sy@gmail.com>
2014-09-03 11:10:59 +0530

    Updated

--- programs/test/align.c
+++ programs/test/align.c
@@ -3,7 +3,7 @@
 
 #define IS_ALIGNED(val) ((val&3) == 0)
 
-unsigned int align_address(unsigned int addr)
+unsigned int word_align1(unsigned int addr)
 {
 	unsigned int i, cnt = 2;
 	if(IS_ALIGNED(addr)) return addr;
@@ -13,13 +13,20 @@ unsigned int align_address(unsigned int addr)
 	return addr;
 }
 
+unsigned int word_align2(unsigned int addr)
+{
+	while (addr%4) addr++;
+	return addr;
+}
+
 int main(int argc, char *argv[])
 {
 	unsigned int rand_addr, i;
 	for (i=0; i<10; i++) {
 		rand_addr = rand();
-		printf("Random address:  0x%x\n", rand_addr);
-		printf("Aligned address: 0x%x\n\n", align_address(rand_addr));
+		printf("Random address:  	0x%x\n", rand_addr);
+		printf("Aligned address 1: 	0x%x\n", word_align1(rand_addr));
+		printf("Aligned address 2: 	0x%x\n\n", word_align2(rand_addr));
 	}
 	return 0;
 }
